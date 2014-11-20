82a2fae49e175b02aff7446d4748f059381482ea
robinkun <robinkun96@gmail.com>
2014-11-20 09:41:02 +0900

    add initalize of order of pressing switch

--- bakudan/bakudan.c
+++ bakudan/bakudan.c
@@ -15,5 +15,17 @@ int init_bakudan(bakudan* game) {
 }
 
 int make_bakudan(bakudan* game) {
+  int i;
+
   game->bomb_loc = rand() % game->player_num;
+
+  // スイッチを押す順番の初期化
+  for(i = 0; i < game->player_num; i++) game->order[i] = i;
+  for(i = 0; i < game->player_num; i++) {
+    int temp;
+    int random = rand() % game->player_num;
+    temp = game->order[random];
+    game->order[random] = game->order[i];
+    game->order[i] = temp;
+  }
 }
