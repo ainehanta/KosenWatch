feb693e758fdb3a37aa04128edab29fad3463c53
robinkun <robinkun96@gmail.com>
2014-12-04 09:55:19 +0900

    status構造体をstatusでtypedef

--- status_data/status.h
+++ status_data/status.h
@@ -3,12 +3,12 @@
 #define INCLUDED_STATUS
 #define MAX_NAME_LENGTH
 
-struct status {
+typedef struct status {
   int health;
   int intelli;
   int turn;
   char name[MAX_NAME_LENGTH];
-};
+} status;
 
 int input_data(const char* filename);
 int output_data(const char* filename);
