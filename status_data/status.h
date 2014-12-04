6de6101c0bca66a4adf179325c9cc1a86b4fc6d1
robinkun <robinkun96@gmail.com>
2014-12-04 09:37:25 +0900

    status構造体追加

--- status_data/status.h
+++ status_data/status.h
@@ -1,7 +1,12 @@
 #ifndef INCLUDED_STATUS
 #define INCLUDED_STATUS
+#define MAX_NAME_LENGTH
 
 struct status {
+  int health;
+  int intelli;
+  int turn;
+  char name[MAX_NAME_LENGTH];
 };
 
 #endif
