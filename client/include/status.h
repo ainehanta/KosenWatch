0cfd0860777c9fe9f0392d31073675ef3bc27222
robinkun <robinkun96@gmail.com>
2014-12-11 09:43:19 +0900

    クライアント用のstatus追加

--- client/include/status.h
+++ client/include/status.h
@@ -1,6 +1,13 @@
-#ifndef __STATUS_H
-#define __STATUS_H
+// status¹½Â¤ÂÎ
+#ifndef INCLUDED_STATUS
+#define INCLUDED_STATUS
+#define MAX_NAME_LENGTH 10
 
-
+typedef struct status {
+  int health;
+  int intelli;
+  int turn;
+  char name[MAX_NAME_LENGTH];
+} status;
 
 #endif
