15ec87695256f42161d13d5553636cdf46f5f955
ainehanta <ainehanta@hotmail.co.jp>
2015-01-08 09:58:00 +0900

    Character系移動

--- client/include/character.h
+++ client/include/character.h
@@ -1,6 +1,9 @@
 #ifndef __CHARACTER_H
 #define __CHARACTER_H
 
-
+void setting(struct parameter player[]);
+void first_judge(struct parameter player[]);
+void final_judge(struct parameter player[]);
+void print_menu(struct parameter player[],int *action);
 
 #endif
