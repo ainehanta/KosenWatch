dc1849dacfa33b37d083d98f776c27ec53a7881c
ainehanta <ainehanta@hotmail.co.jp>
2015-01-15 09:45:05 +0900

    character.cの部分マージ

--- client/include/character.h
+++ client/include/character.h
@@ -1,9 +1,15 @@
 #ifndef __CHARACTER_H
 #define __CHARACTER_H
 
-void setting(struct parameter player[]);
-void first_judge(struct parameter player[]);
-void final_judge(struct parameter player[]);
-void print_menu(struct parameter player[],int *action);
+#define SINKA_FIRST 0
+#define SINKA_FINAL 1
+
+void setting(status *player);
+void print_character(status *player);
+int judge(status *player);
+
+// PRIVATE
+void _first_judge(status *player);
+void _final_judge(status *player);
 
 #endif
